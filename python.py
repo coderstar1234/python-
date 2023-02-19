6ac292a5326e0b2ef2c56d0f48434ccc035f2747
himanshu vashistha <113257378+coderstar1234@users.noreply.github.com>
2023-02-19 09:11:47 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,91 @@
+# Created by Suparna Das
+
+
+
+
+# Cobra 🐍🐍🐍 Python Animation
+
+
+
+
+# Please wait for sometime for the animation to begin
+
+
+
+
+
+
+
+
+
+co = """
+            .#:#:#:#:#:#:#.
+           .#'#.       .#'#.
+           #. '#.     .#' .#     
+           #'● '#.   .# ●'#
+           .'#' '#. .#' '#'.
+            #_'#'*#.#*'#'_#
+            #__  *'#'*  __#
+             #__   |   __#'
+              #__  ^  __#"       
+               #__   __#'    
+                #_   _#    
+                #:    #      
+                #:    #                        
+         .======#:    #=====. 
+%:      *.___ _#:____#'____.*   
+  %::   )%%%%%%%:::::   (
+     %:  /%%%%:::::        \\          
+     %:: %%%:::             
+       %:%:                                                          
+
+""" 
+
+
+
+print('''<style>
+body{background-color:#000024; height: 650px; border: 13px ridge #ffffff; margin:15px}
+b{color:#f0ff00;position:absolute;animation : 3s ease-in 0s sss infinite;animation-direction:alternate;
+margin-left: 15px} 
+         
+@keyframes sss { 0%{font-size:8px;}25%{font-size:11px}50%{font-size: 14px}100% {font-size:8px; color:brown;} }img{display : none}
+marquee{color: #2ee60e;margin-left:20px;margin-right:20px;
+ }
+ 
+</style>
+<b><br><br><br>'''+co+'''</b>'''+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              
+
+<marquee><h1>COBRA....</h1></marquee>''')
+
+
+
+open('.png','w').write('')
+
+
